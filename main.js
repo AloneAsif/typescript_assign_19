d3e1264968b484c7a79634245ba6456e2e65d655
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-10 22:11:40 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question no 20 
+let my_Fav_things = ['mount everset', 'chocolates', 'video games', 'paris', 'kaghan'];
+console.log(my_Fav_things);
+for (let fav of my_Fav_things) {
+    console.log(fav);
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Question no 20 
+
+let my_Fav_things = ['mount everset', 'chocolates', 'video games', 'paris', 'kaghan']
+
+console.log(my_Fav_things)
+
+for(let fav of my_Fav_things){
+    console.log(fav)
+}
